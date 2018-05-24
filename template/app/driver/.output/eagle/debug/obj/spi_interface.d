4094f677ecaf0a3235d33c882c3d212c92e9c33d
XZMicheal <Micheal_Wang1990@163.com>
2018-05-24 14:57:09 +0800

    esp8266 测试demo

---
+++ template/app/driver/.output/eagle/debug/obj/spi_interface.d
@@ -0,0 +1,11 @@
+.output/eagle/debug/obj/spi_interface.o .output/eagle/debug/obj/spi_interface.d : spi_interface.c ../include/spi_interface.h \
+ ../include/spi_register.h \
+ /cygdrive/e/Micheal/eclipse/esp8266/ESP8266_RTOS_SDK-2.0.0/include/espressif/c_types.h \
+ /opt/xtensa-lx106-elf/lib/gcc/xtensa-lx106-elf/4.8.2/include/stdint.h \
+ /opt/xtensa-lx106-elf/lib/gcc/xtensa-lx106-elf/4.8.2/include/stdbool.h \
+ /opt/xtensa-lx106-elf/lib/gcc/xtensa-lx106-elf/4.8.2/include/stddef.h \
+ /cygdrive/e/Micheal/eclipse/esp8266/ESP8266_RTOS_SDK-2.0.0/include/espressif/esp8266/eagle_soc.h \
+ /cygdrive/e/Micheal/eclipse/esp8266/ESP8266_RTOS_SDK-2.0.0/include/espressif/esp8266/ets_sys.h \
+ /cygdrive/e/Micheal/eclipse/esp8266/ESP8266_RTOS_SDK-2.0.0/include/espressif/esp8266/pin_mux_register.h \
+ /cygdrive/e/Micheal/eclipse/esp8266/ESP8266_RTOS_SDK-2.0.0/include/espressif/esp8266/eagle_soc.h \
+ /cygdrive/e/Micheal/eclipse/esp8266/ESP8266_RTOS_SDK-2.0.0/include/espressif/esp_libc.h
